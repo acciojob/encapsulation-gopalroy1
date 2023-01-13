abef6bc34ec852438a0131edd34b01f37cfaf64e
Gopal <gopalroy5397@gmail.com>
2023-01-13 13:06:29 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+//        r.random = 0;
+//        System.out.println(r.random);
+//        'random' has private access in 'com.driver.RWOnly'
+        r.setter(70);
+        System.out.println(r.getter());
+
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int random=9;
+    public int getter(){
+        return this.random;
+    }
+
+    public void setter(int number){
+        random = number;
+    }
 }
